2fa188c7efe0c9bfd173d444817727cffc2c7e70
Matty-Montoya <mmontoya92@gmail.com>
2018-05-16 16:14:22 -0400

    Finish grown button modal
    -Finish adding grow button modal that will open up to showcase code for
    html and css for that effect.
    -Created sections to divide between button animation types
    -Create Nav bar that links to section. Will make it sticky
    -Add page scroll code that will allow the page to scroll to selected
    section.

--- assets/scripts/buttons/events.js
+++ assets/scripts/buttons/events.js
@@ -2,12 +2,20 @@
 
 // const ui = require('./ui')
 
-const onClickGrow = (event) => {
-  $('modal').modal('toggle')
+const onCSSClick = (event) => {
+  $('.css-content').removeClass('hidden')
+  $('.html-content').addClass('hidden')
+}
+
+const onHTMLClick = (event) => {
+  $('.html-content').removeClass('hidden')
+  $('.css-content').addClass('hidden')
 }
 
 const addHandlers = () => {
-
+  $('modal').modal('toggle')
+  $('#grow-css').click(onCSSClick)
+  $('#grow-html').click(onHTMLClick)
 }
 
 module.exports = {
